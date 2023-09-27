40f50a2d1de206b14257ee17102e475568e0da70
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-09-27 21:31:57 +0700

    refactor: move view.php

--- src/app/controllers/Controller.php
+++ src/app/controllers/Controller.php
@@ -13,7 +13,7 @@ abstract class Controller
 
     protected function getView($data = [])
     {
-        require_once __DIR__ . '/../views/view.php';
+        require_once __DIR__ . '/../views/template/view.php';
         return new View($this->folder_path, $data);
     }
 
