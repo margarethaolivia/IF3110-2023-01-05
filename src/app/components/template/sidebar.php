cebbdb50c20dd0200e7827c478987a0666f3f45c
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-09-30 19:24:43 +0700

    fix: typo

--- src/app/components/template/sidebar.php
+++ src/app/components/template/sidebar.php
@@ -1,3 +1,13 @@
-<div>
-    Ini Sidebar
+<div class="sidebar ">
+    <div>
+
+    </div>
+    <div class="layt  ut w-full h-screen">
+        <div>
+            sw
+        </div>
+        <div>
+            sw
+        </div>
+    </div>
 </div>
