b1250dae46d97da10a45433e8ea526769dff560c
Margaretha Olivia <margarethaolivia41@gmail.com>
2023-10-02 16:05:11 +0700

    feat: my videos page

--- src/app/views/myvideos/body.php
+++ src/app/views/myvideos/body.php
@@ -1,10 +1,108 @@
 <?php
+
+include_once (__DIR__ . '/../../components/template/pagination.php');
+
 function body($data) {
-?>    
-
-<div>
-    content my videos
-</div>
-
-<?php 
+?>
+    <main class="pb-2">
+        <div class="flex items-center">
+            <div class="w-big">
+                <h2 class="text-bold">My Videos</h2>
+            </div>
+            <div class="w-small">
+                <span class="black-button">Upload</span>
+            </div>
+        </div>
+        
+        <section id="video-list" class="mb-2">
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="video-card">
+                <img src="https://t4.ftcdn.net/jpg/05/18/69/85/360_F_518698520_Xk9tIwoYpyX6kkVsF6GpQ1z7sKXO8YRz.jpg" alt="Video Thumbnail">
+                <div class="flex">
+                    <div class="w-full ml-2">
+                        <h2 class="text-bold">Soothing 24-hour playlist of jazz music for work</h2>
+                        
+                        <div class="flex justify-between items-center">
+                            <p class="my-1">Uploaded 25/09/2023</p>
+                            <button class="black-button-outline">Edit</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+        <?php 
+            pagination(10);
+        ?>
+    </main>
+<?php
 }
