91415a8a58a9329730c4e1f8414216bfc2d59d08
Enliven26 <16521443@mahasiswa.itb.ac.id>
2023-10-07 13:04:46 +0700

    refactor: add alt to images

--- src/app/components/elements/emptyList.php
+++ src/app/components/elements/emptyList.php
@@ -2,7 +2,7 @@
 function emptyList($image_src, $desc) {
 ?>
     <div class="flex flex-row w-full items-center">
-        <img class="empty-image" src="<?=$image_src?>">
+        <img alt="error image" class="empty-image" src="<?=$image_src?>">
         <span class="error-page-desc"><?=$desc?></span>
     </div>
 <?php
--- src/app/views/error/body.php
+++ src/app/views/error/body.php
@@ -4,7 +4,7 @@
 function body($data) {
 ?>
     <div class="flex flex-row w-full items-center h-screen justify-center">
-        <img class="error-image" src="<?=$data['src']?>">
+        <img alt="error image" class="error-image" src="<?=$data['src']?>">
         <span class="error-page-desc"><?=$data['desc']?></span>
         <a href="<?=$data['link']?>" class="error-page-back">Go Back</a>
     </div>
